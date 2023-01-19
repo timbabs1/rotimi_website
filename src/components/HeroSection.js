f4d5cb0c2e6333b603a5a74ab57363b777c8c84d
timbabs1 <timbabs1@gmail.com>
2023-01-19 22:57:04 +0000

    took out user contract address from hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,9 +1,7 @@
 import classes from './css/HeroSection.module.css'
 //import heroVideo from "../components/assets/hero-video.mp4";
-import { useAccount } from 'wagmi'
 
 function HeroSection() {
-    const { address } = useAccount()
     return (
         <>
             <div className={classes.hero}>
@@ -11,9 +9,6 @@ function HeroSection() {
                 <div className={classes.gradient}></div>
                 <div className={classes.mediaContainer}></div>
                 <button>Join the Mint!</button>
-            </div>
-            <div>
-                <h1>{address}</h1>
             </div>
         </>
     )
