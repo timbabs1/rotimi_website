2e1c35ae814fda18c9aed48dba4ceea2191e046b
timbabs1 <timbabs1@gmail.com>
2023-01-19 00:59:39 +0000

    Integratd first stage minting test

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,15 +1,22 @@
-import classes from "./css/HeroSection.module.css";
+import classes from './css/HeroSection.module.css'
 //import heroVideo from "../components/assets/hero-video.mp4";
+import { useAccount } from 'wagmi'
 
 function HeroSection() {
-  return (
-    <div className={classes.hero}>
-      {/*<video src={heroVideo} autoPlay loop muted />*/}
-      <div className={classes.gradient}></div>
-      <div className={classes.mediaContainer}></div>
-      <button>Join the Mint!</button>
-    </div>
-  );
+    const { address } = useAccount()
+    return (
+        <>
+            <div className={classes.hero}>
+                {/*<video src={heroVideo} autoPlay loop muted />*/}
+                <div className={classes.gradient}></div>
+                <div className={classes.mediaContainer}></div>
+                <button>Join the Mint!</button>
+            </div>
+            <div>
+                <h1>{address}</h1>
+            </div>
+        </>
+    )
 }
 
-export default HeroSection;
+export default HeroSection
--- src/components/MintItem.js
+++ src/components/MintItem.js
@@ -1,7 +1,22 @@
 import classes from '../components/css/MintItem.module.css'
 import { useEffect, useState } from 'react'
 import MintCard from './MintCard'
-import RotimiContract from '../ethereum/rtm'
+//import RotimiContract from '../ethereum/rtm'
+import {
+    useContract,
+    useContractRead,
+    useContractWrite,
+    usePrepareContractWrite,
+    useBalance,
+    useAccount,
+    useWaitForTransaction,
+} from 'wagmi'
+import { useDebounce } from 'usehooks-ts'
+import compiledRotimi from '../ethereum/build/Rotimi_Smart_Contract.json'
+
+//import { useConnectModal, useAccountModal, useChainModal } from '@rainbow-me/rainbowkit'
+import { ethers } from 'ethers'
+import web3 from '../ethereum/web3'
 
 /*
 tier
@@ -15,18 +30,67 @@ benefit
 
 function MintItem({ item }) {
     const [enteredQuantity, setEnteredQuantity] = useState(1)
+    const [mintErrorMessage, setMintErrorMessage] = useState('')
+    const [showMintErrorMessage, setShowMintErrorMessage] = useState(false)
+    const [isMinting, setIsMinting] = useState(false)
+    const [userAccountBalance, setUserAccountBalance] = useState(0)
+    const [cost, setCost] = useState((enteredQuantity * (item.ethValue * 100)) / 100)
+    const [isGeneralError, setIsGeneralError] = useState(false)
+    const debouncedQuantity = useDebounce(item.tier, 500)
+    //const { openConnectModal } = useConnectModal()
+    //const { openAccountModal } = useAccountModal()
+    //const { openChainModal } = useChainModal()
+    const { address: userAddress, isConnecting, isDisconnected } = useAccount()
+    const contract = useContract({ address: process.env.REACT_APP_TEST_NET_ADDRESS, abi: compiledRotimi })
+    const {
+        data: dataFromContract,
+        isError,
+        isLoading: contractReadIsLoading,
+    } = useContractRead({
+        address: process.env.REACT_APP_TEST_NET_ADDRESS,
+        abi: compiledRotimi,
+        functionName: 'totalSupply',
+    })
+    const ethCost = (enteredQuantity * (item.ethValue * 100)) / 100
+    const {
+        config,
+        error: prepareError,
+        isError: isPrepareError,
+    } = usePrepareContractWrite({
+        address: process.env.REACT_APP_TEST_NET_ADDRESS,
+        abi: compiledRotimi,
+        functionName: 'fundRotimi',
+        args: [item.tier, enteredQuantity],
+        enabled: userAccountBalance < ethCost ? false : Boolean(debouncedQuantity),
+        overrides: {
+            from: userAddress,
+            value: ethers.utils.parseEther(ethCost.toString()),
+        },
+    })
 
-    const getTotalSupply = async () => {
-        console.log('REACT_APP_TEST_NET_ADDRESS', process.env.REACT_APP_TEST_NET_ADDRESS)
-        const totalSupply = await RotimiContract.methods.totalSupply().call()
-        console.log(totalSupply)
-    }
+    const { data, error: contractWriteError, isError: contractWriteIsError, write } = useContractWrite(config)
+
+    const { isLoading, isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+    })
+
+    const {
+        data: balance,
+        isError: balanceError,
+        isLoading: balanceLoading,
+    } = useBalance({
+        address: userAddress,
+    })
 
     useEffect(() => {
-        getTotalSupply()
-            .then(r => console.log('r', r))
-            .catch(e => console.log('e', e))
-    }, [])
+        if (balance) {
+            console.log('balance', balance.formatted)
+            setUserAccountBalance(parseInt(balance.formatted))
+        }
+        if (dataFromContract) {
+            console.log('!!!! Total supply', parseInt(String(dataFromContract._hex)))
+        }
+    }, [balance])
 
     function quantityChangeHandler(event) {
         setEnteredQuantity(event.target.value)
@@ -40,6 +104,61 @@ function MintItem({ item }) {
     function decreaseQuantity(event) {
         event.preventDefault()
         enteredQuantity === 1 ? setEnteredQuantity(1) : setEnteredQuantity(enteredQuantity - 1)
+    }
+
+    const handleMintClick = async event => {
+        event.preventDefault()
+
+        await web3.eth.getAccounts(async (error, accounts) => {
+            if (error !== null) {
+                setMintErrorMessage('An error occurred ' + error)
+                setShowMintErrorMessage(true)
+            }
+            // else if (accounts.length === 0) {
+            //     setMintErrorMessage('Please connect your wallet')
+            //     setShowMintErrorMessage(true)
+            // }
+            else {
+                // for prod
+                // if (
+                //   ethereum.networkVersion === '4' ||
+                //   ethereum.networkVersion === '42' ||
+                //   ethereum.networkVersion === '3' ||
+                //   ethereum.networkVersion === '5'
+                // ) {
+                //     setShowMintError(true)
+                //     setMintErrorMessage('You are connected to a test network, please switch network to the mainnet and try again')
+                //     return
+                // }
+                const ethCost = (enteredQuantity * (item.ethValue * 100)) / 100
+                console.log('ethCost', ethCost)
+                if (userAccountBalance < ethCost) {
+                    setMintErrorMessage('Sorry insufficient funds to mint')
+                    setShowMintErrorMessage(true)
+                    setCost(ethCost)
+                    return
+                }
+                setIsMinting(true)
+                setShowMintErrorMessage(false)
+                try {
+                    setCost(ethCost)
+                    const result = await write()
+                    setIsMinting(isLoading)
+                    console.log('result', result)
+                    //setIsMinting(false)
+                    setMintErrorMessage('')
+                    setShowMintErrorMessage(false)
+                } catch (error) {
+                    setIsMinting(false)
+                    setMintErrorMessage('An error occurred' + error)
+                    setShowMintErrorMessage(true)
+                }
+            }
+        })
+        // const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+        // console.log('accounts', accounts)
+        // const result = await RotimiContract.methods.mint(enteredQuantity).send({ from: accounts[0] })
+        // console.log('result', result)
     }
 
     return (
@@ -89,7 +208,33 @@ function MintItem({ item }) {
                                     +
                                 </button>
                             </div>
-                            <button className={classes.mintButton}>Mint</button>
+                            <button
+                                className={classes.mintButton}
+                                disabled={isPrepareError || contractWriteIsError || showMintErrorMessage || !write || isLoading}
+                                onClick={handleMintClick}>
+                                {isLoading ? 'Minting...' : 'Mint'}
+                            </button>
+                            {isSuccess && (
+                                <div>
+                                    Successfully minted your NFT!
+                                    <div>
+                                        {/*<a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>*/}
+                                        <a href={`https://goerli.etherscan.io/tx/${data?.hash}`} target="_blank">
+                                            Etherscan
+                                        </a>
+                                    </div>
+                                </div>
+                            )}
+                            {(isPrepareError || contractWriteIsError || showMintErrorMessage) && (
+                                <>
+                                    {(contractWriteError || mintErrorMessage) && (
+                                        <div>Error: {contractWriteError?.message || mintErrorMessage} minting disabled</div>
+                                    )}
+                                    {prepareError?.reason.includes('insufficient funds') && (
+                                        <div>Error: {prepareError?.reason} minting disabled</div>
+                                    )}
+                                </>
+                            )}
                         </form>
                         <p className={classes.total}>{(enteredQuantity * (item.ethValue * 100)) / 100}ETH</p>
                     </div>
